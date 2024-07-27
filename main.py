9c5127c3b6c464cd67e5187589d5c8c0dfe8fd40
HSE3332 <158248746+HSE3332@users.noreply.github.com>
2024-07-27 14:15:56 +0800

    validation_updated, pls help to make private variables and function

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from maze_algo import MazeAlgo
 turtle.tracer(False)
 
 # Create the maze list
-maze = FileHandler().read_file(inpfilepath='./city.txt').split('\n')
+maze = FileHandler().read_file(inpfilepath='./city2.txt').split('\n')
 print(maze)  # Create the MazeAlgo object
 maze_solver = MazeAlgo(maze)
 
 maze_solver.drawMaze()
+maze_solver.activate_allkeys()
 
 # Keep the turtle window open
 turtle.done()
--- maze_algo.py
+++ maze_algo.py
@@ -2,6 +2,7 @@ import turtle
 from collections import deque
 from draw_box import DrawBox
 
+
 class MazeAlgo:
     def __init__(self, maze, box_size=30):
         self.maze = maze
@@ -25,11 +26,30 @@ class MazeAlgo:
         self.drone.setheading(90)
         self.drone_pos = self.find_drone_start()
         
+        self.following_path=False
         self.paused = False
         self.path_index = 0
-        
-        #listen for key press to move drone
+        self.path_drawn=False
+        self.path_cal=False
+        self.count=0
+        self.text_turtle = turtle.Turtle()  # turtle for text
+        self.text_turtle.hideturtle()
+        # starting text
+        self.draw_text("DRONE STATUS = Manual Mode: Use arrow keys to navigate (press 'f' to calculate shortest path)")
+    def activate_continuekeys(self):
+       self.screen.onkey(turtle.bye, 'q')
+       self.screen.onkey(turtle.bye, 'Q')
+       self.screen.onkey(self.continue_manual, "C")
+       self.screen.onkey(self.continue_manual, "c")
+    def continue_manual(self):
+        self.following_path=False
+        self.activate_allkeys()
+        self.clear_path()
+    def activate_allkeys(self):
+         #listen for key press to move drone
         self.screen.listen()
+        self.screen.onkey(turtle.bye, 'q')
+        self.screen.onkey(turtle.bye, 'Q')
         self.screen.onkey(self.move_up, "Up")
         self.screen.onkey(self.move_down, "Down")
         self.screen.onkey(self.move_left, "Left")
@@ -42,11 +62,54 @@ class MazeAlgo:
         self.screen.onkey(self.toggle_pause, "p")
         self.screen.onkey(self.clear_path, "C")
         self.screen.onkey(self.clear_path, "c")
-
-        self.text_turtle = turtle.Turtle()  # turtle for text
-        self.text_turtle.hideturtle()
-        # starting text
-        self.draw_text("DRONE STATUS = Manual Mode: Use arrow keys to navigate (press 'f' to calculate shortest path)")
+        self.screen.onkey(self.toggle_path_drawing, "H")
+        self.screen.onkey(self.toggle_path_drawing, "h")
+        self.screen.onkey(self.reset, "R")
+        self.screen.onkey(self.reset, "r")
+    def activate_pausedkeys(self):
+        self.screen.onkey(turtle.bye, 'q')
+        self.screen.onkey(turtle.bye, 'Q')
+        self.screen.onkey(self.move_up, "Up")
+        self.screen.onkey(self.move_down, "Down")
+        self.screen.onkey(self.move_left, "Left")
+        self.screen.onkey(self.move_right, "Right")
+        self.screen.onkey(self.toggle_pause, "P")
+        self.screen.onkey(self.toggle_pause, "p")
+        self.screen.onkey(self.reset, "R")
+        self.screen.onkey(self.reset, "r")
+        
+    def deactivate_keys(self):
+        self.screen.onkey(None, "Up")
+        self.screen.onkey(None, "Down")
+        self.screen.onkey(None, "Left")
+        self.screen.onkey(None, "Right")
+        self.screen.onkey(None, "F") #upper and lower case
+        self.screen.onkey(None, "f")
+        self.screen.onkey(None, "C")
+        self.screen.onkey(None, "c")
+        self.screen.onkey(None, "H")
+        self.screen.onkey(None, "h")
+        self.screen.onkey(None, "R")
+        self.screen.onkey(None, "r")
+    def recheck_path(self):
+        if not self.paused:
+            if self.path_cal and self.path_drawn:
+                self.clear_path()
+                self.count=0
+            if self.path_cal:
+                self.clear_path()
+            if self.path_drawn:
+                self.count=0
+                self.clear_path()
+                self.toggle_path_drawing()
+       
+    def toggle_path_drawing(self):
+        self.path_drawn=True
+        if self.count % 2 == 0:
+            self.draw_shortest_path()
+        else:
+            self.clear_path()
+        self.count += 1
 
     def find_drone_start(self):
         for y in range(len(self.maze)):
@@ -85,6 +148,7 @@ class MazeAlgo:
             self.drone.setheading(90)
             self.drone.sety(self.drone.ycor() + self.box_size)
             turtle.update()
+            self.recheck_path()
 
     def move_down(self):
         x, y = self.drone_pos
@@ -93,6 +157,7 @@ class MazeAlgo:
             self.drone.setheading(270)
             self.drone.sety(self.drone.ycor() - self.box_size)
             turtle.update()
+            self.recheck_path()
 
     def move_left(self):
         x, y = self.drone_pos
@@ -101,6 +166,7 @@ class MazeAlgo:
             self.drone.setheading(180)
             self.drone.setx(self.drone.xcor() - self.box_size)
             turtle.update()
+            self.recheck_path()
 
     def move_right(self):
         x, y = self.drone_pos
@@ -109,6 +175,7 @@ class MazeAlgo:
             self.drone.setheading(0)
             self.drone.setx(self.drone.xcor() + self.box_size)
             turtle.update()
+            self.recheck_path()
 
     def bfs_shortest_path(self):
         start_x, start_y = self.drone_pos # get current position
@@ -159,16 +226,20 @@ class MazeAlgo:
             print("No path found!")
 
     def calculate_and_display_path(self):
+        self.path_cal = True
         self.draw_shortest_path()
         self.update_text("DRONE STATUS = Automatic Pilot: Press 'g' to follow pre-calculated path")
 
     def start_following_path(self):
-        self.path_index = 0 # reset the path index as begining
-        self.update_text("DRONE STATUS = Automatic Pilot: Following pre-calculated path. Press 'p' to toggle pause/resume")
-        self.follow_path()
+        if self.path_cal:
+            self.path_index = 0 # reset the path index as begining
+            self.update_text("DRONE STATUS = Automatic Pilot: Following pre-calculated path. Press 'p' to toggle pause/resume")
+            self.deactivate_keys()
+            self.follow_path()
 
     def follow_path(self):
         if self.shortest_path and not self.paused:
+            self.following_path = True
             if self.path_index < len(self.shortest_path):
                 x, y = self.shortest_path[self.path_index]
                 current_x, current_y = self.drone_pos
@@ -199,15 +270,20 @@ class MazeAlgo:
                 steps = len(self.shortest_path) - 1  # initial point not counted
                 dest_x, dest_y = destination[1], destination[0]  # switch x and y, if not the destination is opposite
                 self.update_text(f"DRONE STATUS = Automatic Pilot: Destination ({dest_x}, {dest_y}) reached in {steps} steps. Press 'c' to continue")
+                self.activate_continuekeys()
         elif self.paused:
+            self.activate_pausedkeys()
             self.screen.ontimer(self.follow_path, 100)  # check every 100 ms if paused
 
     def toggle_pause(self):
-        self.paused = not self.paused
-        if self.paused:
-            print("Paused")
-        else:
-            print("Resumed")
+        if self.following_path:
+            self.paused = not self.paused
+            if self.paused:
+                self.activate_pausedkeys()
+                print("Paused")
+            else:
+                self.deactivate_keys()
+                print("Resumed")
 
     def clear_path(self):
          # same as def draw_shortest_path()
@@ -228,6 +304,8 @@ class MazeAlgo:
                                            (len(self.maze) - y) * self.box_size - self.y_offset, 
                                            self.color_map[self.maze[y][x]])
             turtle.update()
+        self.path_drawn=False
+        self.path_cal=False
         self.update_text("DRONE STATUS = Manual Mode: Use arrow keys to navigate (press 'f' to calculate shortest path)")
 
     def draw_text(self, text):
@@ -241,3 +319,9 @@ class MazeAlgo:
 
     def update_text(self, text):
         self.draw_text(text)
+
+    def reset(self):
+        x,y = self.find_drone_start()
+        self.drone_pos = x,y
+        self.drone.setheading(90)
+        turtle.update() 
